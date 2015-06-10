5bd95d5491466226354de86e72c0a8320486b892
TimelessJY <samantha-shan@Hotmail.com>
2015-06-10 12:50:03 +0100

    Implemented drop down menu in the home scene.

--- webapp/MainFeaturesVC.swift
+++ webapp/MainFeaturesVC.swift
@@ -14,16 +14,15 @@ class MainFeaturesVC: UIViewController {
     
     @IBOutlet weak var menuButt: UIBarButtonItem!
     
-    var isAnimating: Bool = false
-    var dropDownViewIsDisplayed: Bool = false
-    
+    @IBOutlet weak var dropDownMenu: UITableView!
+
+    var menus:[String] = ["Load Clothing", "Combine with another", "Share to Friends"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
                // change width of navigation bar
             navigateBar.frame=CGRectMake(0, 0, 400, 100)
-
             self.view .addSubview(navigateBar)
         
         
@@ -33,12 +32,46 @@ class MainFeaturesVC: UIViewController {
         
         menuButt.setBackgroundImage(UIImage(named: "menu_icon"), forState: .Normal, barMetrics: .Default)
         
-        
+        dropDownMenu.hidden = true
         /*
 setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
 [UIColor colorWithRed:245.0/255.0 green:245.0/255.0 blue:245.0/255.0 alpha:1.0], NSForegroundColorAttributeName,
 shadow, NSShadowAttributeName,
 [UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:21.0], NSFontAttributeName, nil]];*/
+        
+        dropDownMenu.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.menus.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = dropDownMenu.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
+        
+        cell.textLabel?.text = self.menus[indexPath.row]
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        
+        return cell
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        switch (indexPath.row) {
+        case 0:
+            self.performSegueWithIdentifier("goto_load", sender: self)
+        case 1:
+            self.performSegueWithIdentifier("goto_combine", sender: self)
+        case 2:
+            self.performSegueWithIdentifier("goto_share", sender: self)
+        default: ()
+        }
     }
 
 
@@ -48,31 +81,31 @@ shadow, NSShadowAttributeName,
     }
 
 
-    /*
+    
     @IBAction func menuButtonTapped(sender: UIBarButtonItem?) {
-        if (self.dropDownViewIsDisplayed) {
-            self.hideDropDownView()
-        } else {
+        if (dropDownMenu.hidden) {
             self.showDropDownView()
+        } else {
+            
+            self.hideDropDownView()
         }
     }
     
     func hideDropDownView() {
-        var frame:CGRect = self.dropDownView.frame
+        var frame:CGRect = self.dropDownMenu.frame
         frame.origin.y = -frame.size.height
-        self.animateDropDownToFrame(frame) {
-            self.dropDownViewIsDisplayed = false
-        }
+        dropDownMenu.hidden = true
+        //self.animateDropDownToFrame(frame) {}
     }
     
     func showDropDownView() {
-        var frame:CGRect = self.dropDownView.frame
+        var frame:CGRect = self.dropDownMenu.frame
         frame.origin.y = self.navigateBar.frame.size.height
-        self.animateDropDownToFrame(frame) {
-            self.dropDownViewIsDisplayed = true
-        }
+        dropDownMenu.hidden = false
+       // self.animateDropDownToFrame(frame) {}
+        
     }
-    
+    /*
     func animateDropDownToFrame(frame: CGRect, completion:() -> Void) {
         if (!self.isAnimating) {
             self.isAnimating = true
@@ -89,3 +122,4 @@ shadow, NSShadowAttributeName,
         }
     }*/
 }
+
